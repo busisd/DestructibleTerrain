dd5a91fc1a3c193ef630596d27775a3daaebaae8
Daniel Busis <danblee123@gmail.com>
2020-09-21 23:43:20 -0500

    Bouncy fireballs!

--- public/terrain.js
+++ public/terrain.js
@@ -35,20 +35,59 @@ app.stage.addChild(new1);
 
 function circleFor(baseX, baseY, radius, fxn) {
   const radiusSquared = radius ** 2;
+  dataArr = [];
+
   for (let curY=baseY-radius; curY<=baseY+radius; curY++) {
     const curWidth = Math.floor(Math.sqrt(radiusSquared - (baseY-curY)**2));
 
     for (let curX=baseX-curWidth; curX<=baseX+curWidth; curX++) {  
-      fxn(curX, curY);
+      fxn(curX, curY, dataArr);
     }
   }
+
+  return dataArr;
 }
 
+const avgPoint2D = (points) => {
+  finalPoint = [0,0];
+  for (point of points) {
+    finalPoint[0] += point[0];
+    finalPoint[1] += point[1];
+  }
+
+  finalPoint[0]/=points.length;
+  finalPoint[1]/=points.length;
+
+  return finalPoint;
+}
+
+const dotProd2D = (vec1, vec2) => (
+  vec1[0]*vec2[0] + vec1[1]*vec2[1]
+);
+
+const multiplyVect2D = (vec, num) => (
+  [vec[0]*num, vec[1]*num]
+);
+
+const vectMagnitude2D = vec => (
+  Math.sqrt(vec[0]**2 + vec[1]**2)
+);
+
+const normalizeVect2D = vec => {
+  const magnitude = vectMagnitude2D(vec);
+  return [vec[0]/magnitude, vec[1]/magnitude];
+}
+
+const subtractVect2D = (vec1, vec2) => (
+  [vec1[0]-vec2[0], vec1[1]-vec2[1]]
+)
+
+const pointsToVect = (p1, p2) => (
+  [p2[0]-p1[0], p2[1]-p1[1]]
+);
 
 const fireballGraphics = new PIXI.Graphics();
 app.stage.addChild(fireballGraphics);
-
-
 class FireBall {
   constructor(xPos, yPos) {
     this.xVelocity = 0;
@@ -74,11 +113,37 @@ class FireBall {
       return;
     }
 
-    circleFor(Math.floor(this.x), Math.floor(this.y), this.radius, (x, y) => {
+    const data = circleFor(Math.floor(this.x), Math.floor(this.y), this.radius, (x, y, dataArr) => {
       if (mapContext.getImageData(x,y,1,1).data[3] > 0) {
-        this.alive = false;
+        dataArr.push([x,y]);
       }
     });
+
+    if (dataArr.length > 0) {
+      const collisionPoint = avgPoint2D(dataArr);
+      const bounceVect = pointsToVect(collisionPoint, [this.x, this.y]);
+
+      const bounceVectMagnitude = vectMagnitude2D(bounceVect);
+      if (bounceVectMagnitude < .05 * this.radius) {
+        this.xVelocity = 0;
+        this.yVelocity = 0;
+        return;
+      }
+
+      const bounceVectNormalized = normalizeVect2D(bounceVect);
+      const dotProd = dotProd2D([this.xVelocity, this.yVelocity], bounceVectNormalized);
+      const multipliedBounceVect = multiplyVect2D(bounceVectNormalized, 2*dotProd);
+      const finalVelocity = subtractVect2D([this.xVelocity, this.yVelocity], multipliedBounceVect);
+      const finalVelocityLossOfEnergy = multiplyVect2D(finalVelocity, 0.7);
+
+      this.xVelocity = finalVelocityLossOfEnergy[0];
+      this.yVelocity = finalVelocityLossOfEnergy[1];
+
+      const bounceVectCorrection = multiplyVect2D(bounceVect, 1 - (bounceVectMagnitude/(this.radius+1)));
+      console.log(bounceVectMagnitude, this.radius, 1 - (bounceVectMagnitude/(this.radius+1)));
+      this.x += bounceVectCorrection[0];
+      this.y += bounceVectCorrection[1];
+    }
   }
 
   move() {
@@ -118,8 +183,7 @@ app.stage.on(
   }
 );
 
-const fb1 = new FireBall(20, 100);
-var fireballList = [fb1];
+var fireballList = [];
 
 app.ticker.add((delta) => {
   fireballGraphics.clear();
